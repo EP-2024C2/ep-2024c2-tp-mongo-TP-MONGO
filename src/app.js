eaaf73cfe4c5cd71f5c15b0056c226a72db62230
Branko Muruaga <muruagabranko253@gmail.com>
2024-11-18 14:16:54 -0300

    fix controllers

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = require("./routes");
+require("dotenv").config();
 const { connectToDatabase } = require("./db/mongo.db");
 
 const app = express();
